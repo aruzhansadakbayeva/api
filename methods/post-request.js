c7eba1499499c0112db84d5781353dcb41b0fc6a
Aruzhan Sadakbayeva <aruuunna.ss@gmail.com>
2023-05-08 16:22:26 +0600

    getHandBookFieldInsp

--- methods/post-request.js
+++ methods/post-request.js
@@ -30,6 +30,12 @@ module.exports = (req, res) => {
           res.writeHead(200, { "Content-Type": "application/json" });
           res.end(JSON.stringify(message));
         }
+        else if (type === "meetingSurvey" && action === "getHandBookFieldInsp") {
+          const message = data.data[type][action];
+
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify(message));
+        }
         else {
           res.writeHead(400, { "Content-Type": "application/json" });
           res.end(
