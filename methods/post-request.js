6f615dcf065cf738d6d21dc04070bda66b7b5463
Aruzhan Sadakbayeva <aruuunna.ss@gmail.com>
2023-05-13 21:42:18 +0600

    uploadFile

--- methods/post-request.js
+++ methods/post-request.js
@@ -15,103 +15,168 @@ module.exports = (req, res) => {
   }
 
   if (req.url === "/api/meetings" && req.method === "POST") {
-    requestBodyparser(req)
-      .then((body) => {
-        const { type, action, visitId, userId} = body;
-        if (type === "plannedMeetingMob" && action === "getMeetings" && userId) {
-          const message = data.data[type][action][userId];
-
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(message));
-        } else if (type === "plannedMeetingMob" && action === "getDetailMeeting" && visitId) {
-          const message = data.data[type][action][visitId];
-
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(message));
-        } 
-        else if (type === "meetingSurvey" && action === "getHandBookWorkDone") {
-          const message = data.data[type][action];
-
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(message));
+    if (req.headers["content-type"].startsWith("multipart/form-data")) {
+      upload.single("file")(req, res, (err) => {
+        if (err) {
+          console.error(err);
+          res.writeHead(500, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ error: "File upload failed" }));
+          return;
         }
-        else if (type === "meetingSurvey" && action === "getHandBookFieldInsp") {
-          const message = data.data[type][action];
 
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(message));
-        }
-        else if (type === "meetingSurvey" && action === "getHandBookContractComplications") {
-          const message = data.data[type][action];
+        const { type, key, src, value } = req.body;
 
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(message));
-        }
-        else if (type === "meetingSurvey" && action === "getHandBookMeetingRecommendations") {
-          const message = data.data[type][action];
+        if ([key === "file" && src && type === "file"], [key === "type" && value ==="uploadFile" && type === "text"], [key === "action" && value ==="visitProfile" && type === "text"], [key === "visitId" && value  && type === "text"]) {
+          const filePath = req.file.path;
+          const { visitId } = req.body;
 
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(message));
-        }
-        else if (type === "plannedMeetingMob" && action === "setStartVisit" && visitId) {
-          const meeting = data.data[type]["getDetailMeeting"][visitId];
-          if (meeting) {
-            meeting.statusVisit = true;
-            meeting.startVisit = new Date().toISOString();
-            const fs = require("fs");
-            const filePath = "./data/meetings.json";
-            const dataToSave = JSON.stringify(data);
-            fs.writeFile(filePath, dataToSave, (err) => {
-              if (err) {
-                console.error("Error saving data:", err);
-                res.writeHead(500, { "Content-Type": "application/json" });
-                res.end(JSON.stringify({ message: "Error saving data" }));
-              } else {
-                res.writeHead(200, { "Content-Type": "application/json" });
-                res.end(JSON.stringify({ message: "startdate fixed" }));
-              }
+          // Read the existing meetings data from the JSON file
+          fs.readFile("./data/meetings.json", (err, data) => {
+            if (err) {
+              console.error(err);
+              res.writeHead(500, { "Content-Type": "application/json" });
+              res.end(JSON.stringify({ error: "Failed to read meetings data" }));
+              return;
+            }
+
+            // Parse the JSON data into a JavaScript object
+            const meetingsObj = JSON.parse(data);
+
+            // Push the uploaded file to the appropriate array
+            meetingsObj.data.plannedMeetingMob.uploadFile.push({
+              name: req.file.originalname,
+              path: filePath,
+              date: new Date().toISOString(),
+              visitId: visitId
             });
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({
-              message: "Start date fixed",
-              meeting: meeting
-            }));
-          }
-        }
-        else if (type === "plannedMeetingMob" && action === "setFinishVisit" && visitId) {
-          const meeting = data.data[type]["getDetailMeeting"][visitId];
-          if (meeting) {
-            meeting.statusVisit = false;
-            meeting.finishVisit = new Date().toISOString();
-            const fs = require("fs");
-            const filePath = "./data/meetings.json";
-            const dataToSave = JSON.stringify(data);
-            fs.writeFile(filePath, dataToSave, (err) => {
-              if (err) {
-                console.error("Error saving data:", err);
-                res.writeHead(500, { "Content-Type": "application/json" });
-                res.end(JSON.stringify({ message: "Error saving data" }));
-              } else {
-                res.writeHead(200, { "Content-Type": "application/json" });
-                res.end(JSON.stringify({ message: "startdate fixed" }));
-              }
-            });
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({
-              message: "Finish date fixed",
-              meeting: meeting
-            }));
-          }
-        }
-        
 
-        
-        else {
+            // Convert the updated object back to a JSON string and write it to the file
+            fs.writeFile("./data/meetings.json", JSON.stringify(meetingsObj), (err) => {
+              if (err) {
+                console.error(err);
+                res.writeHead(500, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ error: "Failed to write meetings data" }));
+                return;
+              }
+
+              // Send the response
+              res.writeHead(200, { "Content-Type": "application/json" });
+              res.end(JSON.stringify({ success: true }));
+            });
+          });
+        } else if (
+          key === "type" &&
+          value === "uploadFile" &&
+          type === "text"
+        ) {
+          // Handle other form data if necessary
+        } else {
           res.writeHead(400, { "Content-Type": "application/json" });
           res.end(
             JSON.stringify({
               title: "Validation Failed",
               message: "Request body is not valid",
+            })
+          );
+        }
+      });
+    } else {
+      requestBodyparser(req)
+        .then((body) => {
+          const { type, action, visitId, userId} = body;
+          if (type === "plannedMeetingMob" && action === "getMeetings" && userId) {
+            const message = data.data[type][action][userId];
+
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(message));
+          }
+          else if (type === "plannedMeetingMob" && action === "getDetailMeeting" && visitId) {
+            const message = data.data[type][action][visitId];
+  
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(message));
+          } 
+          else if (type === "meetingSurvey" && action === "getHandBookWorkDone") {
+            const message = data.data[type][action];
+  
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(message));
+          }
+          else if (type === "meetingSurvey" && action === "getHandBookFieldInsp") {
+            const message = data.data[type][action];
+  
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(message));
+          }
+          else if (type === "meetingSurvey" && action === "getHandBookContractComplications") {
+            const message = data.data[type][action];
+  
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(message));
+          }
+          else if (type === "meetingSurvey" && action === "getHandBookMeetingRecommendations") {
+            const message = data.data[type][action];
+  
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(message));
+          }
+          else if (type === "plannedMeetingMob" && action === "setStartVisit" && visitId) {
+            const meeting = data.data[type]["getDetailMeeting"][visitId];
+            if (meeting) {
+              meeting.statusVisit = true;
+              meeting.startVisit = new Date().toISOString();
+              const fs = require("fs");
+              const filePath = "./data/meetings.json";
+              const dataToSave = JSON.stringify(data);
+              fs.writeFile(filePath, dataToSave, (err) => {
+                if (err) {
+                  console.error("Error saving data:", err);
+                  res.writeHead(500, { "Content-Type": "application/json" });
+                  res.end(JSON.stringify({ message: "Error saving data" }));
+                } else {
+                  res.writeHead(200, { "Content-Type": "application/json" });
+                  res.end(JSON.stringify({ message: "startdate fixed" }));
+                }
+              });
+              res.writeHead(200, { "Content-Type": "application/json" });
+              res.end(JSON.stringify({
+                message: "Start date fixed",
+                meeting: meeting
+              }));
+            }
+          }
+          else if (type === "plannedMeetingMob" && action === "setFinishVisit" && visitId) {
+            const meeting = data.data[type]["getDetailMeeting"][visitId];
+            if (meeting) {
+              meeting.statusVisit = false;
+              meeting.finishVisit = new Date().toISOString();
+              const fs = require("fs");
+              const filePath = "./data/meetings.json";
+              const dataToSave = JSON.stringify(data);
+              fs.writeFile(filePath, dataToSave, (err) => {
+                if (err) {
+                  console.error("Error saving data:", err);
+                  res.writeHead(500, { "Content-Type": "application/json" });
+                  res.end(JSON.stringify({ message: "Error saving data" }));
+                } else {
+                  res.writeHead(200, { "Content-Type": "application/json" });
+                  res.end(JSON.stringify({ message: "startdate fixed" }));
+                }
+              });
+              res.writeHead(200, { "Content-Type": "application/json" });
+              res.end(JSON.stringify({
+                message: "Finish date fixed",
+                meeting: meeting
+              }));
+            }
+          }
+           
+          else {
+            res.writeHead(400, { "Content-Type": "application/json" });
+            res.end(
+              JSON.stringify({
+                title: "Validation Failed",
+                message: "Request body is not valid",
             })
           );
         }
@@ -126,77 +191,9 @@ module.exports = (req, res) => {
           })
         );
       });
-  } else {
+  }} else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ title: "Not Found", message: "Route not found" }));
   }
 };
 
-
-
-module.exports = (req, res) => {
-  if (!res || !res.writeHead) {
-    console.error("Invalid response object provided");
-    return;
-  }
-
-  if (req.url === "/api/meetings" && req.method === "POST") {
-    upload.single("file")(req, res, (err) => {
-      if (err) {
-        console.error(err);
-        res.writeHead(500, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "File upload failed" }));
-        return;
-      }
-
-      const { type, key, src, value } = req.body;
-      
-      //let meetingsData = [];
-
-      if ([key === "file" && src && type === "file"], [key === "type" && value ==="uploadFile" && type === "text"], [key === "action" && value ==="visitProfile" && type === "text"], [key === "visitId" && value  && type === "text"]) {
-        const filePath = req.file.path;
-        const { visitId } = req.body;
-        // Read the existing meetings data from the JSON file
-        fs.readFile("./data/meetings.json", (err, data) => {
-          if (err) {
-            console.error(err);
-            res.writeHead(500, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ error: "Failed to read meetings data" }));
-            return;
-          }
-
-          // Parse the JSON data into a JavaScript object
-          const meetingsObj = JSON.parse(data);
-
-          // Push the uploaded file to the appropriate array
-          meetingsObj.data.plannedMeetingMob.uploadFile.push({
-            name: req.file.originalname,
-            path: filePath,
-            date: new Date().toISOString(),
-            visitId: visitId
-          });
-
-          // Convert the updated object back to a JSON string and write it to the file
-          fs.writeFile("./data/meetings.json", JSON.stringify(meetingsObj), (err) => {
-            if (err) {
-              console.error(err);
-              res.writeHead(500, { "Content-Type": "application/json" });
-              res.end(JSON.stringify({ error: "Failed to write meetings data" }));
-              return;
-            }
-
-            // Send the response
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ success: true }));
-          });
-        });
-      } else if (
-        key === "type" &&
-        value === "uploadFile" &&
-        type === "text"
-      ) {
-        // Handle other form data if necessary
-      }
-    });
-  }
-};
--- util/body-parser.js
+++ util/body-parser.js
@@ -1,17 +1,16 @@
 module.exports = async (request) => {
-    return new Promise((resolve, reject) => {
-      let body = "";
-      request.on("data", (chunk) => {
-        body += chunk;
-      });
-      request.on("end", () => {
-        try {
-          resolve(JSON.parse(body));
-        } catch (err) {
-          console.log(err);
-          reject(new Error("Invalid JSON"));
-        }
-      });
+  return new Promise((resolve, reject) => {
+    let body = "";
+    request.on("data", (chunk) => {
+      body += chunk;
     });
-  };
-  
+    request.on("end", () => {
+      try {
+        resolve(JSON.parse(body));
+      } catch (err) {
+        console.log(err);
+        reject(new Error("Invalid JSON"));
+      }
+    });
+  });
+};
